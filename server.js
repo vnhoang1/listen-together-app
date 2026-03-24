5dac8fbbdcd3d37ce8446f25e56e66329c3fa617
vnhoang1 <131522374+vnhoang1@users.noreply.github.com>
2026-03-24 11:59:10 +0700

    server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+console.log('ENV CHECK:', {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY ? 'SET' : 'MISSING',
+  api_secret: process.env.CLOUDINARY_API_SECRET ? 'SET' : 'MISSING'
+});
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
